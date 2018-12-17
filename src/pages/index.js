32d7b3dcb55ae10d827852bf05c5b4bada07b953
Taras Mankovski <tarasm@gmail.com>
2018-12-17 14:01:33 -0500

    Added frontpage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,127 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
 
-export default class IndexPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
+export default function IndexPage({ data: { allMarkdownRemark, allSimplecastEpisode } }) {
+  let posts = allMarkdownRemark.edges.map(({ node }) => node);
+  let episodes = allSimplecastEpisode.edges.map(({ node }) => node);
+  return (
+    <Layout>
+      <section>
+        <strong>
+          Build a better platform—without the missteps, timeline setbacks, or{" "}
+          <i>even regressions</i>.
+        </strong>
+        <p>We help your team build web applications at scale.</p>
+      </section>
+      <section>
+        <div class="column">
+          <p>
+            When your framework decisions today will impact the state of your
+            product—and team—for years to come, we’ve got your back.
+          </p>
+          <p>
+            Frontside is a small team with deep experience in web UI platform
+            integrations. Since 2005, we’ve been helping engineering teams build
+            rock-solid web applications—and assembling the toolkits that support
+            them.
+          </p>
+        </div>
+        <div class="column">
+          <Link to="services">See how we can Help</Link>
+        </div>
+      </section>
+      <section>
+        <h3>Trusted by top development teams</h3>
+        <ul>
+          <li>
+            <img src="" alt="Honeywell logo" />
+          </li>
+          <li>
+            <img src="" alt="Dell logo" />
+          </li>
+          <li>
+            <img src="" alt="Conde Nast logo" />
+          </li>
+          <li>
+            <img src="" alt="EBSCO logo" />
+          </li>
+          <li>
+            <img src="" alt="SXSW logo" />
+          </li>
+          <li>
+            <img src="" alt="Standard Chartered Bank logo" />
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3>React. Ember. Angular. And many more.</h3>
+        <p>
+          We’re here to make your large-scale application projects run smoothly.
+        </p>
+        <Link to="about">Get to know us</Link>
+      </section>
+      <section>
+        <div class="column">
+          <h3>Latest on the blog</h3>
+          <ul>
+            {posts.map(post => (
+              <li key={post.id}>
+                <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+                <p>
+                  Published by{" "}
+                  {post.fields.authors.map(author => (
+                    <Link to={author.fields.slug}>
+                      {author.frontmatter.name}
                     </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
+                  ))}{" "}
+                  on {post.frontmatter.date}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div class="column">
+          <h3>Latest on the podcast</h3>
+          <ul>
+            {episodes.map(episode => (
+              <li key={episode.id}>
+                <Link to={`/podcast/${episode.slug}`}>{episode.title}</Link>
+                <p>
+                  Published by{" "}
+                  {episode.authors.map(author => (
+                    <Link to={author.fields.slug}>
+                      {author.frontmatter.name}
                     </Link>
-                  </p>
-                </div>
-              ))}
-          </div>
-        </section>
-      </Layout>
-    )
-  }
+                  ))}{" "}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
+      edges: PropTypes.array
     }),
-  }),
-}
+    allSimplecastEpisode: PropTypes.shape({
+      edges: PropTypes.array
+    })
+  })
+};
 
-export const pageQuery = graphql`
+export const indexPageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      limit: 5
     ) {
       edges {
         node {
@@ -66,6 +129,14 @@ export const pageQuery = graphql`
           id
           fields {
             slug
+            authors {
+              frontmatter {
+                name
+              }
+              fields {
+                slug
+              }
+            }
           }
           frontmatter {
             title
@@ -75,5 +146,24 @@ export const pageQuery = graphql`
         }
       }
     }
+    allSimplecastEpisode(
+      limit: 5
+    ) {
+      edges {
+        node {
+          id
+          title
+          slug
+          authors {
+            frontmatter {
+              name
+            }
+            fields {
+              slug
+            }
+          }
+        }      
+      }
+    }
   }
-`
+`;
