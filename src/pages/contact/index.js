596c92f0465ab0622ac6286819dcd39a732b578b
Taras Mankovski <tarasm@gmail.com>
2018-12-18 15:47:21 -0500

    Upgrade contact form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { navigate } from "gatsby-link";
-import Layout from '../../components/Layout'
+import Layout from "../../components/Layout";
 
 function encode(data) {
   return Object.keys(data)
@@ -39,48 +39,79 @@ export default class Index extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-        <h1>What challenges would you like help with today?</h1>
-        <p>Tell us what’s on your mind, and we’ll be in touch within 24 hours.</p>
-        <form
-          name="contact"
-          method="post"
-          action="/contact/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <div hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </div>
-          <div className="field">
-            <label className="label" htmlFor={"name"} >Your name</label>
-            <div className="control">
-              <input className="input" type={"text"} name={"name"} onChange={this.handleChange} id={"name"} required={true} />
+              <h1>What challenges would you like help with today?</h1>
+              <p>
+                Tell us what’s on your mind, and we’ll be in touch within 24
+                hours.
+              </p>
+              <form
+                name="contact"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor="name">
+                    Your name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      name="name"
+                      onChange={this.handleChange}
+                      id="name"
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor="email">
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="email"
+                      name="email"
+                      onChange={this.handleChange}
+                      id="email"
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor="message">
+                    Message
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name="message"
+                      onChange={this.handleChange}
+                      id="message"
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-link" type="submit">
+                    Send
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-          <div className="field">
-            <label className="label" htmlFor={"email"}>Email</label>
-              <div className="control">
-                <input className="input" type={"email"} name={"email"} onChange={this.handleChange} id={"email"} required={true} />
-              </div>
-          </div>
-          <div className="field">
-            <label className="label" htmlFor={"message"}>Message</label>
-            <div className="control">
-              <textarea className="textarea" name={"message"} onChange={this.handleChange} id={"email"} required={true} />
-            </div>
-          </div>
-          <div className="field">
-            <button className="button is-link" type="submit">Send</button>
-          </div>
-        </form>
-        </div>
-        </div>
         </section>
       </Layout>
     );
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,7 +7,7 @@ export default () => (
           <div className="container">
             <div className="content">
                 <h1>Thank you!</h1>
-                <p>This is a custom thank you page for form submissions</p>
+                <p>We will be in touch in 1 business day.</p>
             </div>
           </div>
           </section>
