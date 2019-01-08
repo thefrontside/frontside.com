7fbea966685d837812bc3bc2a9bb505b100d37ba
Taras Mankovski <tarasm@gmail.com>
2019-01-08 17:21:12 -0500

    Removed purge-css

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,7 +92,6 @@ module.exports = {
       },
     },
     'gatsby-connect-authors',
-    'gatsby-plugin-purgecss', // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
