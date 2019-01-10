4809728c6153023fb8b28df04cc0b2c2a18842ac
Wil Wilsman <wil@frontside.io>
2019-01-10 17:00:33 -0600

    Tweak about page design

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,29 +1,29 @@
 import React from "react";
 import Helmet from "react-helmet";
-import Img from "gatsby-image";
 
 import { Link, graphql } from "gatsby";
 import Layout from "../../components/layout";
 import Hero from "../../components/hero";
 import Text from "../../components/text";
 import Content from "../../components/content";
+import Box from '../../components/box';
 import Button, { ButtonGroup } from "../../components/button";
+import TeamList from '../../components/team-list'
 
 const AboutPage = ({
   data: {
-    allMarkdownRemark,
+    allMarkdownRemark: { edges: team },
     site: {
       siteMetadata: { title }
     }
   }
 }) => {
-  let team = allMarkdownRemark.edges.map(({ node }) => node);
-
   return (
     <Layout>
       <Helmet title={`About | ${title}`} />
 
       <Hero
+        align="center"
         heading={
           <Text>
             Bringing predictability to web application projects since 2005
@@ -32,7 +32,7 @@ const AboutPage = ({
       />
 
       <Content>
-        <Text tag="h2">
+        <Text tag="h2" className="align-center">
           We help you choose the best solution for long-term maintainability
         </Text>
         <Text tag="p">
@@ -52,32 +52,44 @@ const AboutPage = ({
       </Content>
 
       <Content>
-        <Text tag="h2">With Frontside, your team can deploy faster</Text>
-        <Text tag="h3">Deep technical experience</Text>
-        <Text tag="p">
-          Every member of our team has significant experience in web UI
-          platforms. From day one, you’ll get practical guidance on how to
-          navigate interactions between frameworks like React, Angular, Ember,
-          and more—all from specialists who have experienced similar challenges
-          firsthand.
-        </Text>
-        <Text tag="h3">Systems that scale</Text>
-        <Text tag="p">
-          We help development teams multiply their productivity with testing and
-          deployment methods that ensure every line of code is ready for the
-          long haul.
-        </Text>
-        <Text tag="h3">Predictable results</Text>
-        <Text tag="p">
-          By embedding ourselves in your team, we remove the unknowns. Get
-          in-depth insight on your toughest challenges, see real progress every
-          step of the way, and watch your team write the highest quality code of
-          their careers.
-        </Text>
+        <Box direction="column">
+          <Text tag="h2" className="align-center margin-bottom-triple">
+            With Frontside, your team can deploy faster
+          </Text>
+          <Box marginBottom="double">
+            <Text tag="h3">Deep technical experience</Text>
+            <Text tag="p">
+              Every member of our team has significant experience in web UI
+              platforms. From day one, you’ll get practical guidance on how to
+              navigate interactions between frameworks like React, Angular, Ember,
+              and more—all from specialists who have experienced similar challenges
+              firsthand.
+            </Text>
+          </Box>
+          <Box marginBottom="double">
+            <Text tag="h3">Systems that scale</Text>
+            <Text tag="p">
+              We help development teams multiply their productivity with testing and
+              deployment methods that ensure every line of code is ready for the
+              long haul.
+            </Text>
+          </Box>
+          <Box>
+            <Text tag="h3">Predictable results</Text>
+            <Text tag="p">
+              By embedding ourselves in your team, we remove the unknowns. Get
+              in-depth insight on your toughest challenges, see real progress every
+              step of the way, and watch your team write the highest quality code of
+              their careers.
+            </Text>
+          </Box>
+        </Box>
       </Content>
 
       <Content>
-        <Text tag="h2">We’re active members of the open source community</Text>
+        <Text tag="h2" className="align-center">
+          We’re active members of the open source community
+        </Text>
         <Text tag="p">
           When your platform is missing a key tool that doesn’t exist yet, we
           build it ourselves. This means we’re able to create the perfect
@@ -90,22 +102,19 @@ const AboutPage = ({
         </ButtonGroup>
       </Content>
 
-      <Content>
+      <Content align="center">
         <Text tag="h2">Meet the team</Text>
-        <ul>
-          {team.map(person => (
-            <li key={person.frontmatter.name}>
-              <Link to={person.fields.slug}>
-                <Img fixed={person.frontmatter.img.childImageSharp.fixed} />
-                {person.frontmatter.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <TeamList people={team.map(({ node }) => ({
+          name: node.frontmatter.name,
+          slug: node.fields.slug,
+          img: node.frontmatter.img
+        }))}/>
       </Content>
 
       <Content>
-        <Text tag="h2">Feel confident in what you’re building</Text>
+        <Text tag="h2" className="align-center">
+          Feel confident in what you’re building
+        </Text>
         <Text tag="p">
           We’ll help you make the best choices for your platform’s long-term
           health so you can move forward with certainty. Contact us today to get
