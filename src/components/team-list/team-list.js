23c9d21b90df5d87316ff29401d2e2a81e71c1ae
Wil Wilsman <wil@frontside.io>
2019-01-10 17:34:58 -0600

    Design team section

--- src/components/team-list/team-list.js
+++ src/components/team-list/team-list.js
@@ -4,6 +4,13 @@ import { Link } from 'gatsby';
 import Img from "gatsby-image";
 import './team-list.css';
 
+// needed to override default inline style
+const imgStyleOverrides = {
+  display: 'block',
+  height: '100%',
+  width: '100%'
+};
+
 TeamList.propTypes = {
   people: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
@@ -18,8 +25,12 @@ export default function TeamList({ people }) {
       {people.map(person => (
         <li key={person.name}>
           <Link to={person.slug}>
-            <Img fixed={person.img.childImageSharp.fixed}/>
-            <p>{person.name}</p>
+            <Img
+              fixed={person.img.childImageSharp.fixed}
+              style={imgStyleOverrides}
+              className="team-img"
+            />
+            <span className="team-name">{person.name}</span>
           </Link>
         </li>
       ))}
