76be50f9fe0ef4022b9359bb0366b7b585006598
Taras Mankovski <tarasm@gmail.com>
2019-03-04 11:18:25 -0500

    Change 404 to use our components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Content from '../components/content';
+import Text from '../components/text';
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
+    <Content>
+      <Text tag="h1">Not found</Text>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    </Content>
   </Layout>
 )
 
