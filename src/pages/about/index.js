266de99d638739c91535e699db857540858db2ad
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2019-04-11 10:44:50 -0700

    Open source section

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -12,7 +12,8 @@ import TeamList from '../../components/team-list';
 import { Link } from 'gatsby';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faComment } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faComment, faCodeBranch, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import './index.css';
 import logo from '../../img/logo-no-text.svg';
@@ -114,7 +115,7 @@ const AboutPage = ({
         <div className="cta-box">
           <header className="cta-box--header">
             <h2>
-              <Link to='contact'>
+              <Link to="/contact">
                 Let's talk
               </Link>
             </h2>
@@ -124,7 +125,7 @@ const AboutPage = ({
             <p>
               Get in touch with one of our technical leaders to discover how our expertise can bring value to your organization.
             </p>
-            <Link className="cta-box--contact" to='contact'>
+            <Link className="cta-box--contact" to="/contact">
               Contact us <FontAwesomeIcon icon={faArrowRight} />
             </Link>
           </div>
@@ -132,91 +133,117 @@ const AboutPage = ({
       </Content>
 
       <Content>
-        <Text tag="h2">
-          We help you choose the best solution for long-term maintainability
-        </Text>
-        <Text tag="p">
-          Choosing frontend technologies on an as-needed basis can lead to a
-          complicated web of conflicting code.
-        </Text>
-        <Text tag="p">
-          Our goal, on the other hand, is to help you identify and utilize the
-          right tools that will set the stage for your frontend code bases for
-          years to come.
-        </Text>
-        <Text tag="p">
-          So expect longevity. Expect higher quality work than you’ve ever
-          created before, and see what it’s like to have the time to focus on
-          new features instead of triaging bugs.
-        </Text>
+        <h2 className="frontside-separator">
+          <span className="frontside-separator--text">
+            <span>We</span>
+            <img src={logo} alt="Frontside" />
+            <span>Open Source</span>
+          </span>
+        </h2>
+
+        <p className="oss-intro-parragraph">
+          We strive to nourish the javascript ecosystem with libraries and tools to empower developers to build better platforms on the web.
+        </p>
+
+        <div className="oss-projects-list">
+          <div className="oss-project">
+            <h3>
+              <a href="https://github.com/microstates" target="_blank"  rel="noopener noreferrer" className="oss-project--title">
+                Microstates
+              </a>
+            </h3>
+            <p>
+              Immutable state primitives for building domain specific models that work equally well in all frameworks.
+            </p>
+            <footer>
+              <a href="https://github.com/microstates" target="_blank"  rel="noopener noreferrer" className="oss-project--github-link">
+                <FontAwesomeIcon icon={faGithub} />
+                <span className="oss-project--stars-label">
+                  <FontAwesomeIcon icon={faStar} />
+                  1,236
+                </span>
+              </a>
+            </footer>
+          </div>
+          <div className="oss-project">
+            <h3>
+              <a href="https://bigtestjs.io" target="_blank"  rel="noopener noreferrer" className="oss-project--title">
+                Big Test
+              </a>
+            </h3>
+            <p>
+              Functional testing toolchain to create test suites that support testing applications consisting of multiple frameworks in a single application.
+            </p>
+            <footer>
+              <a href="https://github.com/bigtestjs" target="_blank"  rel="noopener noreferrer" className="oss-project--github-link">
+                <FontAwesomeIcon icon={faGithub} />
+                <span className="oss-project--stars-label">
+                  <FontAwesomeIcon icon={faStar} />
+                  85
+                </span>
+              </a>
+            </footer>
+          </div>
+          <div className="oss-project">
+            <h3>
+              <a href="https://github.com/thefrontside/frontmacs" target="_blank"  rel="noopener noreferrer" className="oss-project--title">
+                Frontmacs
+              </a>
+            </h3>
+            <p>
+              A package-based, web-centric, customizable, awesome-by-default, acceptance-tested Emacs distribution.
+            </p>
+            <footer>
+              <a href="https://github.com/thefrontside/frontmacs" target="_blank"  rel="noopener noreferrer" className="oss-project--github-link">
+                <FontAwesomeIcon icon={faGithub} />
+                <span className="oss-project--stars-label">
+                  <FontAwesomeIcon icon={faStar} />
+                  405
+                </span>
+              </a>
+            </footer>
+          </div>
+          <div className="oss-project">
+            <h3>
+              <a href="https://github.com/thefrontside/funcadelic.js" target="_blank"  rel="noopener noreferrer" className="oss-project--title">
+                funcadelic.js
+              </a>
+            </h3>
+            <p>
+              Haskell typeclasses and functional primitives for JavaScript
+            </p>
+            <footer>
+              <a href="https://github.com/thefrontside/funcadelic.js" target="_blank"  rel="noopener noreferrer" className="oss-project--github-link">
+                <FontAwesomeIcon icon={faGithub} />
+                <span className="oss-project--stars-label">
+                  <FontAwesomeIcon icon={faStar} />
+                  158
+                </span>
+              </a>
+            </footer>
+          </div>
+        </div>
+
+        <div className="cta-box">
+          <header className="cta-box--header">
+            <h2>
+              <Link to="/contact">
+                Contribute
+              </Link>
+            </h2>
+            <FontAwesomeIcon icon={faCodeBranch} />
+          </header>
+          <div className="cta-box--body">
+            <p>
+            Reach out if you have feedback or questions about our projects, or just want to talk about open source.
+            </p>
+            <Link className="cta-box--contact" to="/contact">
+              Contact us <FontAwesomeIcon icon={faArrowRight} />
+            </Link>
+          </div>
+        </div>
       </Content>
 
-      <Content>
-        <Box direction="column">
-          <Text tag="h2" className="margin-bottom-triple">
-            With Frontside, your team can deploy faster
-          </Text>
-          <Box marginBottom="double">
-            <Text tag="h3">Deep technical experience</Text>
-            <Text tag="p">
-              Every member of our team has significant experience in web UI
-              platforms. From day one, you’ll get practical guidance on how to
-              navigate interactions between frameworks like React, Angular, Ember,
-              and more—all from specialists who have experienced similar challenges
-              firsthand.
-            </Text>
-          </Box>
-          <Box marginBottom="double">
-            <Text tag="h3">Systems that scale</Text>
-            <Text tag="p">
-              We help development teams multiply their productivity with testing and
-              deployment methods that ensure every line of code is ready for the
-              long haul.
-            </Text>
-          </Box>
-          <Box>
-            <Text tag="h3">Predictable results</Text>
-            <Text tag="p">
-              By embedding ourselves in your team, we remove the unknowns. Get
-              in-depth insight on your toughest challenges, see real progress every
-              step of the way, and watch your team write the highest quality code of
-              their careers.
-            </Text>
-          </Box>
-        </Box>
-      </Content>
-
-      <Content>
-        <Text tag="h2">
-          We’re active members of the open source community
-        </Text>
-        <Text tag="p">
-          When your platform is missing a key tool that doesn’t exist yet, we
-          build it ourselves. This means we’re able to create the perfect
-          solution for your organization while contributing to the open source
-          community. Our latest tool is <a href="https://bigtestjs.io">BigTest</a>, designed to test single
-          page apps faster than any similar solutions available today.
-        </Text>
-        <ButtonGroup>
-          <Button to="/work">
-            <Text>Learn more about our work</Text>
-          </Button>
-        </ButtonGroup>
-      </Content>
-
-      <Content>
-        <Text tag="h2">
-          Feel confident in what you’re building
-        </Text>
-        <Text tag="p">
-          We’ll help you make the best choices for your platform’s long-term
-          health so you can move forward with certainty. Contact us today to get
-          practical guidance on your toughest platform challenges.
-        </Text>
-        <ButtonGroup>
-          <Button to="/contact">Get Started</Button>
-        </ButtonGroup>
-      </Content>
     </Layout>
   );
 };
