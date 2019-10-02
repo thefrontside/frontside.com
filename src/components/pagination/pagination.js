a4d4c7a0a12d4feab117dfc8bae106f9cfad0ff3
Taras Mankovski <tarasm@gmail.com>
2019-10-02 17:10:41 -0400

    Change direction of newer/older in pagination

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
+import './pagination.css';
 
 export default function Pagination({ prefix, page, pages }) {
   return (
     <div className="pagination">
-      {page > 1 ? (
-        <Link to={page === 2 ? prefix : `${prefix}/${page - 1}`}>
-          &larr; Newer
-        </Link>
-      ) : null}
       {page + 1 !== pages ? (
-        <Link to={`${prefix}/${page + 1}`}>Older &rarr;</Link>
+        <Link to={`${prefix}/${page + 1}`}>&larr; Older</Link>
+      ) : null}
+      {page > 1 ? (
+        <Link className="pagination-newer-link" to={page === 2 ? prefix : `${prefix}/${page - 1}`}>
+          Newer &rarr;
+        </Link>
       ) : null}
     </div>
   );
