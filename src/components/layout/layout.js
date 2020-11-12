9394f7824dc2cf084ec56df388da61818cfdeee6
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2020-11-12 15:51:38 +0100

    Makes webflow.js path refer to /

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -93,7 +93,7 @@ export default function TemplateWrapper({
               }
             />
             <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f7f19d60c33ef0c409a8bf8" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-            <script src="js/webflow.js" type="text/javascript"></script>
+            <script src="/js/webflow.js" type="text/javascript"></script>
           </Helmet>
           <Navbar />
 
