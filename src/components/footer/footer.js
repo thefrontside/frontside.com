4f2701b00cadbf1d0ae6c9e1d0a3923588c6a912
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2020-11-13 13:08:08 +0100

    Fixes gatsby's warnings

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -13,9 +13,9 @@ export default function Footer() {
         <a href="/consulting" className="footerlink">Consulting</a>
         <a href="/tools" className="footerlink">Tools</a>
         <a href="/blog" aria-current="page" className="footerlink w--current">Insights</a>
-        <a href="#" className="footerlink">Contact</a>
+        <a href="/contact" className="footerlink">Contact</a>
       </nav>
-      <a href="https://frontside.com/" target="_blank" className="footer-symbol"><img src={FSIcon} loading="lazy" width="32.5" alt="" className="image-6" /></a>
+      <a href="/" className="footer-symbol"><img src={FSIcon} width="32.5" alt="" className="image-6" /></a>
       <p className="footer-notes"><br />2301 W Anderson Ln #107<br />Austin, Texas 78757<br />+1 (800) 493-4589</p>
       <p className="copyright">{`\u00A9 2005-${year} The Frontside Software, Inc. All rights reserved.`}</p>
     </footer>
