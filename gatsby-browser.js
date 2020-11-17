73485ed0c75f1462907636751d3ef38cec1d5943
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2020-11-17 09:41:30 +0100

    Fixes issue with corssorigin field

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@ exports.onInitialClientRender = () => {
     jQScript.src = "https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f7f19d60c33ef0c409a8bf8";
     jQScript.integrity = "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=";
     jQScript.type = "text/javascript";
-    jQScript.crossorigin = "anonymous";
+    jQScript.crossOrigin = "anonymous";
     document.body.appendChild(jQScript);
 
     const wfScript = document.createElement('script');
