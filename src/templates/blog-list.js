858350eab688b610e875fb131b5c8a91eb31dba0
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2021-01-27 11:59:40 +0100

    Podcast prompt styles

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
+import { Helmet } from "react-helmet";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Layout from '../components/layout';
 import PostsList from '../components/posts-list';
 import Pagination from '../components/pagination';
 
 import BlogHeroImage from '../img/plork/blog-hero@1.5x.png';
+import ApplePodcastsBadge from '../img/apple-podcasts-badge.svg';
 
 import './blog-list.css';
 
@@ -91,8 +94,13 @@ export default function BlogPage({
     formattedPosts = postsAndEpisodes;
   }
 
+  const [copied, setCopied] = useState(false);
+
   return (
     <Layout title={page === 1 ? 'Blog' : `Blog - page ${page}`}>
+      <Helmet>
+        <link rel="alternate" type="application/rss+xml" title="The Frontside Podcast RSS" href="https://rss.simplecast.com/podcasts/96/rss" />
+      </Helmet>
       <section className="widewrapper herowrapper w-container">
         <div className="herotext">
           <h1 className="heading">Sharing <span className="gradient-text">Frontside's</span> latest discoveries</h1>
@@ -110,6 +118,24 @@ export default function BlogPage({
         </div>
         <div className="consultingheroimage">
           <img src={BlogHeroImage} alt="" />
+        </div>
+      </section>
+      <section className="widewrapper w-container podcast-block">
+        <h2 className="podcast-title">
+          Subscribe to <br /> our podcast:
+        </h2>
+        <a className="applepodcasts-link" href="https://podcasts.apple.com/us/podcast/the-frontside-podcast/id827250386?itsct=podcast_box&amp;itscg=30200" target="_blank" rel="noopener noreferrer">
+          <img src={ApplePodcastsBadge} alt="Listen on Apple Podcasts" />
+        </a>
+        <div className="rss-box">
+          <CopyToClipboard text='https://rss.simplecast.com/podcasts/96/rss'
+            onCopy={() => setCopied(true)}>
+            <button className="fs-button action">Copy RSS</button>
+          </CopyToClipboard>
+          <div className="input-boder">
+            <input className="fs-text-field" value="https://rss.simplecast.com/podcasts/96/rss" />
+          </div>
+          <span className={`rss-box--copied ${copied ? 'rss-box--copied__done' : 'rss-box--copied__pending'}`}>&#x2713;</span>
         </div>
       </section>
       <PostsList
