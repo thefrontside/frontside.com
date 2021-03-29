e2b9708e752e744677b3bc5fbe4deca874cdbdc6
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2021-03-29 13:25:30 +0200

    Removes bad trackers, introduces good tracker

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,7 @@ const React = require("react")
 
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <link rel="stylesheet" href="https://use.typekit.net/gyc5wys.css" />
+    <link rel="stylesheet" href="https://use.typekit.net/gyc5wys.css" />,
+    <script async="" defer="" data-domain="frontside.com" src="https://plausible.io/js/plausible.js"></script>
   ])
 }
