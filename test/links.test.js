2fa3b0720db5e529ee0c36346f04365185d1598e
Jacob Bolda <me@jacobbolda.com>
2021-07-28 13:43:21 -0500

    feat: bigtest (#143)
    * init bigtest and first test
    
    * add homepage and link click tests
    
    * run bigtest in ci
    
    * start up gatsby in ci through bigtest
    
    * try BigTest from gatsby serve
    
    * gatsby serve doesn't seem to serve webflow properly
    
    * try chrome.headless
    
    * headless mode must have mobile resolution, click menu button
    
    * only run bigtest locally

---
+++ test/links.test.js
@@ -0,0 +1,33 @@
+import { test, Page, HTML, Link, Heading, not, and, including } from 'bigtest';
+
+export default test('homepage')
+  .step(
+    Page.visit('/'),
+    HTML({ classList: including('menu-button-2') }).click()
+  )
+  .assertion(Link('People').exists())
+  .assertion(Link('Consulting').exists())
+  .assertion(Link('Tools').exists())
+  .assertion(Link('Blog & Podcast').exists())
+  .child('clicking People', test =>
+    test
+      .step(Link('People').click())
+      .assertion(Heading('MEET THE TEAM').exists())
+  )
+  .child('clicking Consulting', test =>
+    test
+      .step(Link('Consulting', { className: not('footerlink') }).click())
+      .assertion(
+        Heading('BUILD THE ENGINEERING BACKBONE OF AN INDUSTRY LEADER').exists()
+      )
+  )
+  .child('clicking Tools', test =>
+    test
+      .step(Link('Tools', { className: not('footerlink') }).click())
+      .assertion(Heading('OPEN SOURCE TOOLS TO MAKE TESTING EASIER').exists())
+  )
+  .child('clicking Blog & Podcast', test =>
+    test
+      .step(Link(and(including('Blog'), including('Podcast'))).click())
+      .assertion(Heading("SHARING FRONTSIDE'S LATEST DISCOVERIES").exists())
+  );
