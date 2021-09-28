acfd9a7748900ca7500d782e1f81c559d9713a31
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2021-09-28 14:28:34 +0200

    Fix SSR issue with reduced motion plugin

--- src/components/animation.tsx
+++ src/components/animation.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import { useInView } from 'react-hook-inview';
-import { useReduceMotion } from 'react-reduce-motion';
+
+const NORMAL_SPEED = 1;
+const REDUCED_SPEED = 0.5;
 
 export default function Animation({ src, className = undefined }) {
   const [ref, isVisible] = useInView({
@@ -10,28 +12,55 @@ export default function Animation({ src, className = undefined }) {
 
   const [lotty, setLotty] = useState(null);
 
-  const reduceMotion = useReduceMotion();
+  const reducedMotion = usePrefersReducedMotion();
 
   useEffect(() => {
     if (!!lotty && isVisible) {
-      lotty.play();
-    }
-    if (!!lotty && !isVisible) {
-      lotty.pause();
+      if (isVisible) {
+        lotty.play();
+      } else {
+        lotty.pause();
+      }
     }
   }, [lotty, isVisible]);
 
-  console.info(reduceMotion)
+  useEffect(() => {
+    if (!!lotty) {
+      lotty.setSpeed(reducedMotion ? REDUCED_SPEED : NORMAL_SPEED);
+    }
+  }, [reducedMotion]);
 
   return (
     <div ref={ref} className={className}>
       <Player
         lottieRef={(instance) => setLotty(instance)}
         src={src}
-        speed={reduceMotion ? 0.5 : 1}
+        speed={reducedMotion ? REDUCED_SPEED : NORMAL_SPEED}
         autoplay
         loop
       />
     </div>
   );
+}
+
+// From: https://www.joshwcomeau.com/react/prefers-reduced-motion/#the-hook
+const QUERY = '(prefers-reduced-motion: no-preference)';
+function usePrefersReducedMotion() {
+  // Default to no-animations, since we don't know what the
+  // user's preference is on the server.
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(true);
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+    // Set the true initial value, now that we're on the client:
+    setPrefersReducedMotion(!window.matchMedia(QUERY).matches);
+    // Register our event listener
+    const listener = (event) => {
+      setPrefersReducedMotion(!event.matches);
+    };
+    mediaQueryList.addEventListener('change', listener);
+    return () => {
+      mediaQueryList.removeEventListener('change', listener);
+    };
+  }, []);
+  return prefersReducedMotion;
 }
