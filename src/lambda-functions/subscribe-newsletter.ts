3f099b777b4c9282f277103b5ea8389e972f5967
Jorge Lainfiesta <jorgelainfiesta@users.noreply.github.com>
2022-02-09 15:41:52 +0100

    Switch subscription provider (#191)

--- src/lambda-functions/subscribe-newsletter.ts
+++ src/lambda-functions/subscribe-newsletter.ts
@@ -1,27 +1,27 @@
 import { Handler } from '@netlify/functions';
 import axios from 'axios';
 
-const senderKey = process.env.SENDER_API;
-const url = 'https://api.sender.net/v2/subscribers';
+const mailerliteKey = process.env.MAILERLITE_API;
+const endpoint = 'https://api.mailerlite.com/api/v2';
+const groupId = '110434688';
 
 const handler: Handler = async (event) => {
   const formData = JSON.parse(event.body);
   const email = formData.email;
+  const url = `${endpoint}/groups/${groupId}/subscribers`;
 
   if (!formData || !email) {
     return { statusCode: 422 };
   }
 
-  let headers = {
-    'Authorization': `Bearer ${senderKey}`,
+  const headers = {
+    'X-MailerLite-ApiKey': `${mailerliteKey}`,
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   };
 
   const data = {
     email,
-    status: 'subscribed',
-    groups: ['boVm9A']
   };
 
   try {
@@ -35,6 +35,7 @@ const handler: Handler = async (event) => {
     return { statusCode: 200 };
   }
   catch(error) {
+    console.log(error.response);
     let statusCode = error.response.status;
     console.log(statusCode);
     return { statusCode };
