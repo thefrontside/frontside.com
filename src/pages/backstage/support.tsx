d8f38a436527bce4667dd933bbcc250fad686731
Taras <tarasm@gmail.com>
2023-02-14 14:06:58 -0500

    Cleaned up support page

--- src/pages/backstage/support.tsx
+++ src/pages/backstage/support.tsx
@@ -105,11 +105,13 @@ const Hero = ({ submitted, setSubmitted }) => (
   <header className={heroWrap}>
     <div className={heroText}>
       <h1 className={heading3Xl}>
-        Enterprise Support for <span className={textGradientVioletGreen}>Backstage</span> {' '}
-        by <span className={textGradientVioletGreen}>Frontside</span>
+        Enterprise Support for{' '}
+        <span className={textGradientVioletGreen}>Backstage</span> by{' '}
+        <span className={textGradientVioletGreen}>Frontside</span>
       </h1>
       <p className={textLg}>
-        Accelerate your Backstage adoption with experience, insight and practical advice from Backstage contributors.
+        Accelerate your Backstage adoption with experience, insight and
+        practical advice from Backstage contributors.
       </p>
       <p className={consultingTopTCA}>
         <SupportCTA
@@ -125,50 +127,86 @@ const Hero = ({ submitted, setSubmitted }) => (
       </p>
     </div>
     <div className={heroImage}>
-      <img src={backstageSupportHero} alt="backstage support hero illustration" />
+      <img
+        src={backstageSupportHero}
+        alt="backstage support hero illustration"
+      />
     </div>
   </header>
 );
 
 const Badges = () => (
   <section className={badgesWrap}>
-      <header className={badgesHeader}>
-          <img src={leftLaurel} alt="left side of laurel surrounding the section header" />
-          <div>
-              We Are A Professional
-              <div>
-                  <span className={badgesTextBackstage}>Backstage</span> Services Partner
-              </div>
-          </div>
+    <header className={badgesHeader}>
+      <img
+        src={leftLaurel}
+        alt="left side of laurel surrounding the section header"
+      />
+      <div>
+        We Are A Professional
+        <div>
+          <span className={badgesTextBackstage}>Backstage</span> Services
+          Partner
+        </div>
+      </div>
 
-          <img src={rightLaurel} alt="right side of laurel surrounding the section header" />
-      </header>
-      <p className={badgesBody}>
-          <img className={badgeRibbon} src={backstageRibbon} alt="ribbon showing backstage  achievements" />
+      <img
+        src={rightLaurel}
+        alt="right side of laurel surrounding the section header"
+      />
+    </header>
+    <p className={badgesBody}>
+      <img
+        className={badgeRibbon}
+        src={backstageRibbon}
+        alt="ribbon showing backstage  achievements"
+      />
 
-          <img src={backstageContributorOfMonth} alt="selected contributor of the month"/>
-          <div className={badgeCaption}>Recognized as "Contributor of the Month"</div>
-          <img src={backstageContributionCount} alt="badge showing 34 pull requests" />
-          <div className={badgeCaption}>34 Pull Requests Merge Into Backstage Project</div>
-          <img src={backstageActiveSigParticipants} alt="many file folder active at once" />
-          <div className={badgeCaption}>Active Participant in Backstage Catalog SIG</div>
-          <img src={backstageConPresentationCount} alt="presentation count" />
-          <div className={badgeCaption}>2 Presentations at Backstage Conf</div>
-      </p>
+      <img
+        src={backstageContributorOfMonth}
+        alt="selected contributor of the month"
+      />
+      <div className={badgeCaption}>
+        Recognized as "Contributor of the Month"
+      </div>
+      <img
+        src={backstageContributionCount}
+        alt="badge showing 34 pull requests"
+      />
+      <div className={badgeCaption}>
+        49 Pull Requests Merged Into Backstage Project
+      </div>
+      <img
+        src={backstageActiveSigParticipants}
+        alt="many file folder active at once"
+      />
+      <div className={badgeCaption}>
+        Contributors to Catalog SIG and Adoption SIG
+      </div>
+      <img src={backstageConPresentationCount} alt="presentation count" />
+      <div className={badgeCaption}>2 Presentations at Backstage Conf</div>
+    </p>
   </section>
-)
+);
 
 const ValueProp = () => (
   <section className={pageWrap}>
-      <header className={sectionHeader}>
-          <h2 className={homeBackstageHeading}>
-              Expert Knowledge from
-              <strong className={textGradientVioletGreen}> Leaders in Backstage</strong>
-          </h2>
-      </header>
-          <p className={bodyCopy}>
-              Our team are early adopters, core contributors and leaders in Backstage. They have extensive experience with Backstage and have lead Backstage, Developer Experience and Internal Developer Platform (IDPs) projects that support over 100k developers. They have indispensable knowledge that will help you, your team, and organization reach our Backstage project goals.
-          </p>
+    <header className={sectionHeader}>
+      <h2 className={homeBackstageHeading}>
+        Expert Knowledge from{' '}
+        <strong className={textGradientVioletGreen}>
+          Leaders in Backstage
+        </strong>
+      </h2>
+    </header>
+    <p className={bodyCopy}>
+      We are early adopters, core contributors, and leaders in the Backstage
+      community. We have extensive experience with Backstage and have led
+      Backstage, Developer Experience, and Internal Developer Platform (IDPs)
+      projects that support over 100k developers. We have indispensable
+      knowledge to help you, your team, and your organization reach your
+      Backstage project goals.
+    </p>
   </section>
 );
 
@@ -178,10 +216,15 @@ const LeftRight = () => (
       {/* class for text on right */}
       <div className={featureTextAlternate}>
         <h2 className={featureHeading}>
-          Give your DX Team <span className={textGradientVioletGreen}>A Boost</span>
+          Give your DX Team{' '}
+          <span className={textGradientVioletGreen}>A Boost</span>
         </h2>
         <p className={bodyCopy}>
-          Backstage is a powerful tool because it comes complete with an architecture, many plugins and best practices. With power comes a learning curve that can be difficult to master without help. Frontside gives your team information they need to be productive with Backstage.
+          Backstage is a powerful tool because it comes complete with
+          architecture, many plugins, and best practices. With power comes a
+          learning curve that can be difficult to master without help. Frontside
+          gives your team the information they need to be productive with
+          Backstage.
         </p>
       </div>
       <div className={featureImage}>
@@ -193,10 +236,14 @@ const LeftRight = () => (
       {/* class for text on left */}
       <div className={featureText}>
         <h2 className={featureHeading}>
-          Gain Traction with <span className={textGradientVioletGreen}>Proven Strategies</span>
+          Gain Traction with{' '}
+          <span className={textGradientVioletGreen}>Proven Strategies</span>
         </h2>
         <p>
-          Companies are using Backstage to improve developer experience, impact employee retention and accelerate their Cloud Native adoption strategy. Frontside has seen first hand how successful Backstage adopters design their
+          Companies are using Backstage to improve developer experience, improve
+          employee retention and build Internal Developer Platforms. Frontside
+          has seen firsthand how successful Backstage adopters design their
+          roadmaps.
         </p>
       </div>
       <div className={featureImage}>
@@ -208,10 +255,15 @@ const LeftRight = () => (
       {/* class for text on right */}
       <div className={featureTextAlternate}>
         <h2 className={featureHeading}>
-          Avoiding Forking With <span className={textGradientVioletGreen}>the Lastest Features</span>
+          Avoiding Forking With{' '}
+          <span className={textGradientVioletGreen}>the Lastest Features</span>
         </h2>
         <p>
-          Sometimes Backstage doesn't do exactly what your team needs. Modifying Backstage without pushing changes to Backstage project makes upgrading difficult and locks you out of using the latest features of Backstage. Frontside keeps your team unblocked by adovcating and contributing changes to the Backstage project so yo can always upgrade and get the latest features with ease.
+          When Backstage doesn't do what your team needs, modifying Backstage
+          without applying changes upstream makes upgrading difficult and locks
+          you out of using the latest features of Backstage. Frontside keeps
+          your team unblocked by contributing changes to the Backstage project
+          so you can always upgrade and quickly get the latest features.
         </p>
       </div>
       <div className={featureImage}>
@@ -223,12 +275,16 @@ const LeftRight = () => (
       {/* class for text from left */}
       <div className={featureText}>
         <h2 className={featureHeading}>
-          Be Bold in Face of Ambitious <span className={textGradientVioletGreen}>DX&nbsp;initatives</span>
+          Be Bold in Face of Ambitious{' '}
+          <span className={textGradientVioletGreen}>DX&nbsp;initatives</span>
         </h2>
         <p>
-          Each organization is unique. Solutions to your ambitous initaives may not exsist in the open source Backstage project instead of limitiing your project to what is available in open source, Frontside helps you to take on your ambitious iniative knowing that with Frontside's help you'll be able to successfully accomplish your goals. Frontside can help you architect, build and optimize new Backstage plugins.
+          Each organization is unique. Solutions to your ambitious initiatives
+          may not exist in the open-source Backstage project. With Frontside by
+          your side, you can take on any initiative knowing that your team can
+          reach your goals. Frontside can help your team architect, build and
+          optimize your Backstage portal.
         </p>
-
       </div>
       <div className={featureImage}>
         <img src={backstageValuePropI51} alt="" />
@@ -239,12 +295,17 @@ const LeftRight = () => (
       {/* class for text on right */}
       <div className={featureTextAlternate}>
         <h2 className={featureHeading}>
-          Ship new DX features with <span className={textGradientVioletGreen}>confidence</span>
+          Ship new DX features with{' '}
+          <span className={textGradientVioletGreen}>confidence</span>
         </h2>
         <p>
-          As your Backstage project grows, so withh the risk of breaking exisiting features that your users rely on. Reliable shift-left and shift-right testing practices can prevent frustration for your users by helping you catch problems before they're deployed to your users. Frontside helps DX teams implement integration testing harnesses that give you confidence without blocking your team from shipping new Backstage features.
+          As your Backstage project grows, so does the risk of breaking existing
+          features. Reliable testing practices can catch problems before they
+          impact your users' productivity. Frontside helps DX teams implement
+          Backstage ingestion and integration testing practices that give you
+          confidence without blocking your team from shipping new Backstage
+          features.
         </p>
-
       </div>
       <div className={featureImage}>
         <img src={backstageValuePropI52} alt="" />
@@ -256,29 +317,17 @@ const LeftRight = () => (
 const Trust = () => (
   <section className={pageWrap}>
     <header className={sectionHeader}>
-        <h2 className={featureHeading}>
-          {'Trusted by '}
-          <strong className={textGreen}>Our Clients</strong>
-        </h2>
+      <h2 className={featureHeading}>
+        {'Trusted by '}
+        <strong className={textGreen}>Our Clients</strong>
+      </h2>
     </header>
     <div className={contentRow}>
       <div className={contentGrow}>
         <div className={logoFlex}>
-          <Logo
-            src={clientHpLogo}
-            alt="HP logo"
-            width="55px"
-          />
-          <Logo
-            src={clientFannieMaeLogo}
-            alt="Fannie Mae Logo"
-            width="200px"
-          />
-          <Logo
-            src={clientIndeedLogo}
-            alt="Indeed Logo"
-            width="100px"
-          />
+          <Logo src={clientHpLogo} alt="HP logo" width="55px" />
+          <Logo src={clientFannieMaeLogo} alt="Fannie Mae Logo" width="200px" />
+          <Logo src={clientIndeedLogo} alt="Indeed Logo" width="100px" />
         </div>
       </div>
     </div>
@@ -292,43 +341,40 @@ const Logo = ({ src, alt, width }) => (
 );
 
 const TestimonialCarousel = ({ children }) => (
-  <div className={testimonialCarousel}>
-      {children}
-  </div>
+  <div className={testimonialCarousel}>{children}</div>
 );
 
 const Testimonial = ({ source, children }) => (
   <div className={testimonialWrap}>
-      <div className={testimonialQuoteChar}>&ldquo;</div>
-      <div>
-          <span className={testimonialQuote}>{children}</span>
-          <span className={testimonialSource}>{source}</span>
-      </div>
+    <div className={testimonialQuoteChar}>&ldquo;</div>
+    <div>
+      <span className={testimonialQuote}>{children}</span>
+      <span className={testimonialSource}>{source}</span>
+    </div>
   </div>
-)
+);
 
 const Testimonials = () => (
   <section className={pageWrap}>
-      <header className={sectionHeader}>
-          <h2 className={homeBackstageHeading}>
-              The work we do{' '}
-              <strong className={textGradientVioletGreen}>Transcends</strong>
-          </h2>
-          <p className={textLg}>
-              Our clients speak.
-          </p>
-      </header>
-      <TestimonialCarousel>
-          <Testimonial source="North, at Hewlett-Packard">
-              I learned more working with Frontside for six months than I have in six years.
-          </Testimonial>
-          <Testimonial source="Brian Beale, Engr Manager at Resideo">
-              Frontside has a dedication to engineering excellence.
-          </Testimonial>
-          <Testimonial source="Kaspar for Grünberg, CEO of Humanitec">
-              Hard to find a team more knowledgeable in Backstage than Frontside.
-          </Testimonial>
-      </TestimonialCarousel>
+    <header className={sectionHeader}>
+      <h2 className={homeBackstageHeading}>
+        The work we do{' '}
+        <strong className={textGradientVioletGreen}>Transcends</strong>
+      </h2>
+      <p className={textLg}>Our clients speak.</p>
+    </header>
+    <TestimonialCarousel>
+      <Testimonial source="North, at Hewlett-Packard">
+        I learned more working with Frontside for six months than I have in six
+        years.
+      </Testimonial>
+      <Testimonial source="Brian Beale, Engr Manager at Resideo">
+        Frontside has a dedication to engineering excellence.
+      </Testimonial>
+      <Testimonial source="Kaspar for Grünberg, CEO of Humanitec">
+        Hard to find a team more knowledgeable in Backstage than Frontside.
+      </Testimonial>
+    </TestimonialCarousel>
   </section>
 );
 
@@ -344,15 +390,33 @@ const PlansComparisonChart = () => (
       <thead>
         <tr>
           <th></th>
-          <th><img src={supportTierSilver} alt="" /></th>
-          <th><img src={supportTierGold} alt="" /></th>
-          <th><img src={supportTierPlatnium} alt="" /></th>
+          <th>
+            <img src={supportTierSilver} alt="" />
+          </th>
+          <th>
+            <img src={supportTierGold} alt="" />
+          </th>
+          <th>
+            <img src={supportTierPlatnium} alt="" />
+          </th>
         </tr>
         <tr className={rowBorder}>
           <th className={tableCellLeftHeader}>+Benefits</th>
-          <th className={boldCaps}>Silver<br/>Plan</th>
-          <th className={boldCaps}>Gold<br/>Plan</th>
-          <th className={boldCaps}>Platnium<br/>Plan</th>
+          <th className={boldCaps}>
+            Silver
+            <br />
+            Plan
+          </th>
+          <th className={boldCaps}>
+            Gold
+            <br />
+            Plan
+          </th>
+          <th className={boldCaps}>
+            Platnium
+            <br />
+            Plan
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -363,48 +427,49 @@ const PlansComparisonChart = () => (
           <td>Advanced</td>
         </tr>
         <tr className={rowBorder}>
-          <td className={tableCellLeft}>Recommended Teams <br/>(including Managers)</td>
-          <td>1-2 Devs</td>
-          <td>3 Devs <br /> 2 PMs</td>
-          <td>
-            5 Devs <br />
-            1 Designer <br />
-            2 Managers
+          <td className={tableCellLeft}>
+            Recommended for <br />
+            teams with
           </td>
+          <td>less than 2 developers</td>
+          <td>3 to 5 developers</td>
+          <td>more than 5 developers</td>
         </tr>
         <tr className={rowBorder}>
-          <td className={tableCellLeft}>Standing Core Meeting</td>
-          <td>x</td>
+          <td className={tableCellLeft}>Standing Meetings</td>
+          <td>not included</td>
           <td>1 per week</td>
-          <td>5 - 6 per month</td>
+          <td>2 per week</td>
         </tr>
         <tr className={rowBorder}>
-          <td className={tableCellLeft}>Ad-hoc Working Sessions <br />(Pair Programming or planning)</td>
-          <td>2 per week</td>
+          <td className={tableCellLeft}>
+            Pair programming <br /> sessions
+          </td>
+          <td>1 per week</td>
           <td>3 per week</td>
           <td>20 per month</td>
         </tr>
         <tr className={rowBorder}>
-          <td className={tableCellLeft}>Code Spikes <br />(POC, Code Samples, Pseudo Code)</td>
-          <td>x</td>
-          <td>2 per month</td>
-          <td>4 per month</td>
-        </tr>
-        <tr className={rowBorder}>
           <td className={tableCellLeft}>Code Reviews</td>
-          <td>x</td>
+          <td>not included</td>
           <td>1 per week</td>
           <td>12 per month</td>
         </tr>
         <tr className={rowBorder}>
-          <td className={tableCellLeft}>Rfcs</td>
-          <td>x</td>
-          <td>1 per month</td>
+          <td className={tableCellLeft}>Code examples</td>
+          <td>not included</td>
           <td>2 per month</td>
+          <td>4 per month</td>
+        </tr>
+        <tr className={rowBorder}>
+          <td className={tableCellLeft}>Upstream changes</td>
+          <td>not included</td>
+          <td>1 concurrent</td>
+          <td>2 concurrent</td>
         </tr>
         <tr>
           <td className={tableCellLeft}>Chat Support</td>
-          <td>x</td>
+          <td>1 seat</td>
           <td>2 seats</td>
           <td>4 seats</td>
         </tr>
