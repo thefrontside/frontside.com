5e6f328136f9b9f52456e6e4cfa8f410716614eb
Charles Lowell <cowboyd@frontside.com>
2023-07-14 17:18:59 +0300

    Add navlinks and logo to header

--- nextgen/html/app.html.tsx
+++ nextgen/html/app.html.tsx
@@ -10,6 +10,7 @@ export default function* AppHtml(options: Options): Operation<JSX.Element> {
   let { title, description } = options;
   let siteURL = yield* url();
   let logoNoText = yield* url("assets/fs-logo-no-text.svg");
+  let logoURL = yield* url("assets/fs-logo.svg");
 
   return (
     <html lang="en-US" dir="ltr">
@@ -21,7 +22,7 @@ export default function* AppHtml(options: Options): Operation<JSX.Element> {
         <meta property="og:url" content={yield* url()} />
         <meta property="og:description" content={description} />
         <meta name="description" content={description} />
-        <meta name="twitter:image" content={yield* url("assets/fs-logo.svg")} />
+        <meta name="twitter:image" content={logoURL} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <link rel="icon" href={logoNoText} />
@@ -29,15 +30,18 @@ export default function* AppHtml(options: Options): Operation<JSX.Element> {
         <link rel="alternate" href={siteURL} hreflang="en" />
         <link rel="alternate" href={siteURL} hreflang="x-default" />
       </head>
-      <body>
+      <body class="m-5">
         <header class="w-full">
           <nav aria-label="Site Nav">
-            <ul class="flex items-center gap-6">
-              <NavLink
-                href="/"
-                text="Frontside"
-              />
-            </ul>
+            <img width="137" src={logoURL} alt="Frontside Logo" />
+          </nav>
+          <nav arial-label="Home Nav">
+            <menu>
+              <li><a href="/consulting">DX Consulting</a></li>
+              <li><a href="/backstage">Backstage</a></li>
+              <li><a href="/opensource">Open Source</a></li>
+              <li><a href="/blog">DX Consulting</a></li>
+            </menu>
           </nav>
         </header>
         <main>
@@ -51,18 +55,5 @@ export default function* AppHtml(options: Options): Operation<JSX.Element> {
         </footer>
       </body>
     </html>
-  );
-}
-
-function NavLink({ href, text }: { href: string; text: string }) {
-  return (
-    <li>
-      <a
-        class=" "
-        href={href}
-      >
-        {text}
-      </a>
-    </li>
   );
 }
