7694bc64f5304781c3b2fab4754b1d1698560fac
Dmitriy Lazarev <w@kich.dev>
2023-12-22 01:39:24 +0400

    Make subscribeCTA responsive
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- legacy/src/components/newsletter-subscribe-cta/newsletter-subscribe-cta.css.ts
+++ legacy/src/components/newsletter-subscribe-cta/newsletter-subscribe-cta.css.ts
@@ -9,8 +9,20 @@ import {
 export const newsletterSubscribeCTA = style({
   marginTop: vars.space.md,
   marginBottom: vars.space.xl,
-  marginLeft: vars.space['3xl'],
-  marginRight: vars.space['3xl'],
+  '@media': {
+    'only screen and (min-width: 420px)': {
+      marginLeft: vars.space.xl,
+      marginRight: vars.space.xl,
+    },
+    'only screen and (min-width: 992px)': {
+      marginLeft: vars.space['2xl'],
+      marginRight: vars.space['2xl'],
+    },
+    'only screen and (min-width: 1200px)': {
+      marginLeft: vars.space['3xl'],
+      marginRight: vars.space['3xl'],
+    },
+  },
 });
 
 export const signupButton = style([ actionButton, {
