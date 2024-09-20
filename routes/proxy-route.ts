66f77d47c6203e97e8cc93103402c115bfd703e6
Charles Lowell <cowboyd@frontside.com>
2024-09-19 23:50:29 -0500

    ✨ Proxy the Interactors website
    The Interactors website had been proxied by netlify, but we want to
    take responsibility for serving it ourselves.
    
    This enhances the proxy to take on serving static docusaurus sites.
    Basically, it does two things, it accepts a `root` parameter which
    gives the root path on the proxy site that we start serving from.
    
    Also we enhance the proxy generally so that if it receives a redirect
    to itelf, then it will also proxy the redirect rather than go to the
    source site.

--- routes/proxy-route.ts
+++ routes/proxy-route.ts
@@ -1,17 +1,21 @@
 import type { HTTPMiddleware } from "revolution";
-import { call } from "effection";
+import { call, Operation } from "effection";
 
 export interface ProxyRouteOptions {
   website: string;
   prefix: string;
+  root?: string;
 }
 
 export function proxyRoute(options: ProxyRouteOptions): HTTPMiddleware {
-  return function* proxy(request) {
+  return function* proxy(request): Operation<Response> {
     let website = new URL(options.website);
+
     let target = new URL(request.url);
+
     let prefix = new RegExp(`^\/${options.prefix}\/?`);
-    target.pathname = target.pathname.replace(prefix, "/");
+    target.pathname = target.pathname.replace(prefix, options.root ?? "/");
+
     target.hostname = website.hostname;
     target.port = website.port;
     target.protocol = website.protocol;
@@ -29,6 +33,25 @@ export function proxyRoute(options: ProxyRouteOptions): HTTPMiddleware {
       redirect: "manual",
       headers,
     }));
+
+    if (response.status === 301) {
+      let location = response.headers.get("location");
+      if (location?.startsWith(String(website))) {
+        let headers: Record<string, string> = {};
+        for (let [key, value] of request.headers.entries()) {
+          headers[key] = value;
+        }
+
+        let url = new URL(request.url);
+        headers.location = location.replace(target.origin, url.origin);
+
+        response = new Response(null, {
+          status: response.status,
+          statusText: response.statusText,
+          headers,
+        });
+      }
+    }
 
     return response;
   };
