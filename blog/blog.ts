c615f79a803700e23901ae9bc9e42dd76c8a041a
Elrick Ryan <elrickvm@gmail.com>
2024-12-06 18:23:50 -0500

    Add blog tags

--- blog/blog.ts
+++ blog/blog.ts
@@ -1,7 +1,7 @@
 import { evaluate } from "npm:@mdx-js/mdx@3.1.0";
 import remarkGfm from "npm:remark-gfm@4.0.0";
-import remarkFrontmatter from 'npm:remark-frontmatter@5.0.0';
-import remarkMdxFrontmatter from 'npm:remark-mdx-frontmatter@5.0.0';
+import remarkFrontmatter from "npm:remark-frontmatter@5.0.0";
+import remarkMdxFrontmatter from "npm:remark-mdx-frontmatter@5.0.0";
 import rehypePrismPlus from "npm:rehype-prism-plus@1.5.1";
 import { Fragment, jsx, JSXElement, jsxs } from "revolution/jsx-runtime";
 import { call, createContext, Operation } from "effection";
@@ -17,7 +17,8 @@ export interface BlogPost {
   description: string;
   date: Date;
   author: string;
-  content: () => JSXElement
+  tags: Array<string>;
+  content: () => JSXElement;
 }
 
 interface Frontmatter {
@@ -36,45 +37,54 @@ export function* useBlog(): Operation<Blog> {
 export function* initBlog(): Operation<void> {
   let directory = new URL(import.meta.resolve("./")).pathname;
   let entries = Deno.readDirSync(directory);
-  let matches = [...entries].map((entry) =>
-    [...entry.name.matchAll(/(\d{4})-(\d{2})-(\d{2})-.*\.md$/g)]
-  ).filter(([match]) => !!match).map(([match]) => {
+  let matches = [...entries].map((
+    entry,
+  ) => [...entry.name.matchAll(/(\d{4})-(\d{2})-(\d{2})-.*\.md$/g)]).filter((
+    [match],
+  ) => !!match).map(([match]) => {
     let [filename, yearstring, monthstring, daystring] = match;
-    let date = new Date(Number(yearstring), Number(monthstring) - 1, Number(daystring));
+    let date = new Date(
+      Number(yearstring),
+      Number(monthstring) - 1,
+      Number(daystring),
+    );
     let datestring = `${yearstring}-${monthstring}-${daystring}`;
     let id = filename.slice(0, -3);
     return { filename, date, datestring, id };
   });
 
-  
   let posts = new Map<string, BlogPost>();
-  
+
   for (let match of matches.toReversed()) {
     let { date, id } = match;
     let location = new URL(import.meta.resolve(`../blog/${match.filename}`));
     let source = yield* call(() => Deno.readTextFile(location));
-    let mod = yield* call(() => evaluate(source, {
-      development: true,
-      jsx,
-      jsxs,
-      jsxDEV: jsx,
-      Fragment,
-      remarkPlugins: [
-        remarkFrontmatter, remarkMdxFrontmatter, remarkGfm,
-      ],
-      rehypePlugins: [
-        [rehypePrismPlus, { showLineNumbers: true }],
-      ],
-    }));
-    
+    let mod = yield* call(() =>
+      evaluate(source, {
+        development: true,
+        jsx,
+        jsxs,
+        jsxDEV: jsx,
+        Fragment,
+        remarkPlugins: [
+          remarkFrontmatter,
+          remarkMdxFrontmatter,
+          remarkGfm,
+        ],
+        rehypePlugins: [
+          [rehypePrismPlus, { showLineNumbers: true }],
+        ],
+      })
+    );
+
     posts.set(id, {
       ...mod.frontmatter as Frontmatter,
       id,
       date,
-      content: () => mod.default({})
-    })
+      content: () => mod.default({}),
+    });
   }
- 
+
   yield* BlogContext.set({
     get: (id) => posts.get(id),
     getPosts: () => [...posts.values()],
