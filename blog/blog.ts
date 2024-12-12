86c21d7017a6379e88bcf9c01828f53f5ccc44fc
Charles Lowell <cowboyd@frontside.com>
2024-12-12 11:37:17 -0600

    Put every blog post in its own completely self-contained directory

--- blog/blog.ts
+++ blog/blog.ts
@@ -5,6 +5,7 @@ import remarkMdxFrontmatter from "npm:remark-mdx-frontmatter@5.0.0";
 import rehypePrismPlus from "npm:rehype-prism-plus@1.5.1";
 import { Fragment, jsx, JSXElement, jsxs } from "revolution/jsx-runtime";
 import { call, createContext, Operation } from "effection";
+import { existsSync } from "jsr:@std/fs";
 
 export interface Blog {
   slice(...args: Parameters<Array<unknown>["slice"]>): BlogPost[];
@@ -40,28 +41,31 @@ export function* useBlog(): Operation<Blog> {
 export function* initBlog(): Operation<void> {
   let directory = new URL(import.meta.resolve("./")).pathname;
   let entries = Deno.readDirSync(directory);
-  let matches = [...entries].map((
-    entry,
-  ) => [...entry.name.matchAll(/(\d{4})-(\d{2})-(\d{2})-.*\.md$/g)]).filter((
-    [match],
-  ) => !!match).map(([match]) => {
-    let [filename, yearstring, monthstring, daystring] = match;
-    let date = new Date(
-      Number(yearstring),
-      Number(monthstring) - 1,
-      Number(daystring),
-    );
-    let datestring = `${yearstring}-${monthstring}-${daystring}`;
-    let id = filename.slice(0, -3);
-    return { filename, date, datestring, id };
-  });
+  let matches = [...entries].flatMap((entry) => {
+    let markdownfile = new URL("index.md", import.meta.resolve(`./${entry.name}/`));
+    if (entry.isDirectory && existsSync(markdownfile)) {
+      let [match] = [...entry.name.matchAll(/(\d{4})-(\d{2})-(\d{2})-.*/g)];
+      if (match) {
+	let [dirname, yearstring, monthstring, daystring] = match;
+	let date = new Date(
+	  Number(yearstring),
+	  Number(monthstring) - 1,
+	  Number(daystring),
+	);
+	let datestring = `${yearstring}-${monthstring}-${daystring}`;
+	let id = dirname;
 
+	return [{ markdownfile, dirname, date, datestring, id }];	
+      }
+    } 
+    return [];
+  });
+  
   let posts = new Map<string, BlogPost>();
 
   for (let match of matches.toReversed()) {
     let { date, id } = match;
-    let location = new URL(import.meta.resolve(`../blog/${match.filename}`));
-    let source = yield* call(() => Deno.readTextFile(location));
+    let source = yield* call(() => Deno.readTextFile(match.markdownfile));
     let mod = yield* call(() =>
       evaluate(source, {
         jsx,
@@ -86,12 +90,14 @@ export function* initBlog(): Operation<void> {
       id,
       date,
       content: () => {
-	return mod.default({});
+        return mod.default({});
       },
     });
   }
 
-  let values = [...posts.values()].sort((a, b) => b.date.getTime() - a.date.getTime());
+  let values = [...posts.values()].sort((a, b) =>
+    b.date.getTime() - a.date.getTime()
+  );
 
   yield* BlogContext.set({
     slice: (...args) => values.slice(...args),
