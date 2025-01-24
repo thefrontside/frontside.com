8dd6e6f09bdb8127747d111484f7b6516b6f6f41
Elrick Ryan <elrickvm@gmail.com>
2025-01-24 17:12:29 -0500

    Organize blog post list into three columns
    - Updated layout to display blog posts in three columns
    instead of a single list
    - Improved visual organization for better user experience

--- routes/blog-tag-route.tsx
+++ routes/blog-tag-route.tsx
@@ -6,8 +6,8 @@ export function blogTagRoute() {
   return function* () {
     let blog = yield* useBlog();
 
-    let { tag } = yield* useParams<{ tag: string}>();
-    
+    let { tag } = yield* useParams<{ tag: string }>();
+
     let posts = blog.getPostsByTag(tag);
 
     let App = yield* useAppHtml({
@@ -19,17 +19,32 @@ export function blogTagRoute() {
       twitterXImage: "/assets/index-meta-home-cloud-native.png",
     });
 
+    console.log(posts);
+
     return (
       <App>
-        <div class="prose">
+        <div class="flex flex-col justify-self-center !max-w-none prose prose">
           <section>
-	    <h2>Tag: {tag}</h2>
-            <ol>
-              {posts.map((post) => (
-                <li>
-                  <a href={`/blog/${post.id}`}>{post.title}</a>
-                </li>
-              ))}
+            <h2>Tag: {tag}</h2>
+            <ol class="md:gap-6 lg:gap-11 space-y-10 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 mx-auto p-4 max-w-7xl">
+              {posts.map((post) => {
+                console.log("helloo Post image--- " + post.image);
+                return (
+                  <li class="md:mt-0 p-2 md:p-4 border prose">
+                    <a class="no-underline" href={`/blog/${post.id}`}>
+                      <img
+                        src={post.image
+                          ? `blog/${post.id}/${post.image}`
+                          : "/assets/fs-logo-no-text.svg"}
+                        alt="blog image"
+                      />
+                      <div>
+                        <h3>{post.title}</h3>
+                      </div>
+                    </a>
+                  </li>
+                );
+              })}
             </ol>
           </section>
         </div>
