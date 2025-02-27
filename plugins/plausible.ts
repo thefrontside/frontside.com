c63ae20cf31a4f8684827655bda0fc2d923a5531
Charles Lowell <cowboyd@frontside.com>
2025-02-27 10:44:28 -0600

    ✨ Inject plausible analytics into proxied sites

--- plugins/plausible.ts
+++ plugins/plausible.ts
@@ -1,33 +1,45 @@
-import { RevolutionPlugin } from "revolution";
+import { HASTHtmlNode, RevolutionPlugin } from "revolution";
 import { select } from "npm:hast-util-select";
+import { createContext, type Operation } from "effection";
+import { type Root } from "hast";
 
 export interface PlausibleOptions {
   enabled: boolean;
 }
 
-export function plausiblePlugin(options: PlausibleOptions): RevolutionPlugin {
+const PlausibleContext = createContext<PlausibleOptions>("plausible");
+
+export function* plausiblePlugin(
+  options: PlausibleOptions,
+): Operation<RevolutionPlugin> {
+  yield* PlausibleContext.set(options);
+
   return {
     *html(request, next) {
       let html = yield* next(request);
 
-      if (!options.enabled) {
-        return html;
-      }
-
-      let head = select("head", html);
-
-      head?.children.push({
-        type: "element",
-        tagName: "script",
-        properties: {
-          src: "https://plausible.io/js/script.js",
-          defer: true,
-          "data-domain": "frontside.com",
-        },
-        children: [],
-      });
-
-      return html;
+      return yield* injectPlausible(html);
     },
   };
+}
+
+export function* injectPlausible(html: Root): Operation<HASTHtmlNode> {
+  let { enabled } = yield* PlausibleContext.expect();
+
+  if (enabled) {
+    let head = select("head", html);
+
+    head?.children.push({
+      type: "element",
+      tagName: "script",
+      properties: {
+        src: "https://plausible.io/js/script.js",
+        defer: true,
+        "data-domain": "frontside.com",
+      },
+      children: [],
+    });
+  }
+
+  return html;
 }
